b1e5d03c3cf693c0525d2a4b30ae710eddf9a0ec
regiondavid <uestczjw@gmail.com>
2017-05-18 23:33:18 +0800

    change ueditor

--- ueditor/php/Uploader.class.php
+++ ueditor/php/Uploader.class.php
@@ -110,7 +110,8 @@ class Uploader
 
         //创建目录失败
         if (!file_exists($dirname) && !mkdir($dirname, 0777, true)) {
-            $this->stateInfo = $this->getStateInfo("ERROR_CREATE_DIR");
+            // $this->stateInfo = $this->getStateInfo("ERROR_CREATE_DIR");
+            $this->stateInfo = $dirname;
             return;
         } else if (!is_writeable($dirname)) {
             $this->stateInfo = $this->getStateInfo("ERROR_DIR_NOT_WRITEABLE");
