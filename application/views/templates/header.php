6a75d3234bd5e31602f36cc23617b668d8577b31
regiondavid <uestczjw@gmail.com>
2017-05-28 09:15:28 +0800

    change styke

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link href="http://lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/style/style.v0.3.1.css">
+    <link rel="stylesheet" href="/assets/style/style.v0.3.2.css">
     <script src="http://lib.baomitu.com/jquery/2.2.4/jquery.min.js"></script>
     <script src="http://lib.baomitu.com/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
