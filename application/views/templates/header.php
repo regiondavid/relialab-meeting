4e02bd1746e7bb1cfb0eba071146b2fad970b1a9
regiondavid <uestczjw@gmail.com>
2017-07-02 18:41:29 +0800

    change style

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link href="http://lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/style/style.css?20170702">
+    <link rel="stylesheet" href="/assets/style/style.css?2017070201">
     <script src="http://lib.baomitu.com/jquery/2.2.4/jquery.min.js"></script>
     <script src="http://lib.baomitu.com/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
