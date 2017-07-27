51e2533e7a1ec73763d3179917b230a00c77c2c6
regiondavid <uestczjw@gmail.com>
2017-07-27 12:32:02 +0800

    添加最新标示

--- application/views/news/notice.php
+++ application/views/news/notice.php
@@ -2,7 +2,11 @@
     <span class="more-t">最新通知</span><span class="bor-s"></span>
   </div>
   <ul class="list-group">
-  <?php foreach ($list as $item): ?>
-    <a class="news-li" href=<?php echo "/news/view/".$item['id'] ?>><li class="list-group-item"><?php echo "<span class='badge'>".$item['publishtime']."</span>".$item['title'] ?></li></a>
+  <?php foreach ($list as $key=>$item): ?>
+    if($key<2) {
+        <a class="news-li" href=<?php echo "/news/view/".$item['id'] ?>><li class="list-group-item"><?php echo "<span class='badge'>".$item['publishtime']."</span>".$item['title'] ?></li><span class="new-lable">new</span></a>
+    } else {
+        <a class="news-li" href=<?php echo "/news/view/".$item['id'] ?>><li class="list-group-item"><?php echo "<span class='badge'>".$item['publishtime']."</span>".$item['title'] ?></li></a>
+    }
   <?php endforeach; ?>
   </ul>
